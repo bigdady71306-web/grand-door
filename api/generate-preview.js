2f63d7e5089b468b9841ae87570ee5dec5171113
Grand Doors <granddoorsrefinishing21@gmail.com>
2026-04-04 11:37:32 -0500

    Fix API module syntax - use CommonJS require/module.exports

--- api/generate-preview.js
+++ api/generate-preview.js
@@ -1,6 +1,6 @@
-import Replicate from 'replicate';
+const Replicate = require('replicate');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
